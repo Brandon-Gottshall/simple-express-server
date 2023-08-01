b1dc9c390127ecc5185fe02c66bbc91da9219ac2
Brandon🤖 <brandon@effortlessdevelopment.com>
2023-08-01 18:01:25 -0400

    init server

---
+++ server.js
@@ -0,0 +1,27 @@
+// import dotenv
+dotenv = require('dotenv')
+
+// Setup dotenv
+dotenv.config()
+console.log(process.env)
+
+// Setup Express server
+const express = require('express')
+
+// Setup Express app
+const app = express()
+
+// Setup port
+const PORT = process.env.SERVER_PORT || 3001
+
+// Setup Root Route (For testing)
+
+app.get('/', (req, res) => {
+  res.send('Best quote ever!')
+})
+
+// app.listen
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
