791bcda59e107d3d99473f81e04c673123440119
Brandon🤖 <brandon@effortlessdevelopment.com>
2023-08-01 19:39:19 -0400

    finish routes

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 // import dotenv
 dotenv = require('dotenv')
 
+// import Postgres
+const { Pool } = require('pg')
+
 // Setup dotenv
 dotenv.config()
+
+// Setup Postgres pool
+const pool = new Pool({ connectionString: process.env.DATABASE_URL })
 
 // Setup OpenAI client
 const { OpenAIApi, Configuration } = require('openai')
@@ -22,6 +28,9 @@ const cors = require('cors')
 // Setup cors
 app.use(cors((origin = '*')))
 
+// express.json
+app.use(express.json())
+
 // Setup port
 const PORT = process.env.SERVER_PORT || process.env.PORT || 3001
 
@@ -31,7 +40,7 @@ app.get('/', (req, res) => {
   res.send(JSON.stringify('Best quote ever!'))
 })
 
-app.get('/new/:topic', (req, res) => {
+app.get('/new/:topic', async (req, res) => {
   // Get topic from request
   const topic = req.params.topic
   // OpenAI Chat Completion
@@ -52,6 +61,33 @@ app.get('/new/:topic', (req, res) => {
       console.log(response.data.choices[0].message.content)
       res.send(response.data.choices[0].message.content)
     })
+})
+
+// Save quotes to database
+app.post('/save', async (req, res) => {
+  const client = await pool.connect()
+  // Get quote from req
+  const quote = req.body.quote
+  // Save quote to database
+  await client.query('INSERT INTO quotes (quote) VALUES ($1)', [quote])
+  res.status(200).send('Quote saved!')
+})
+
+// Fetch all quotes
+
+app.get('/quotes', async (req, res) => {
+  const client = await pool.connect()
+  const quotes = await client.query('SELECT * FROM quotes')
+  res.send(quotes.rows)
+})
+
+// Delete quotes
+
+app.delete('/delete/:id', async (req, res) => {
+  const client = await pool.connect()
+  const id = req.params.id
+  await client.query('DELETE FROM quotes WHERE id = $1', [id])
+  res.status(200).send('Quote deleted!')
 })
 
 // app.listen
